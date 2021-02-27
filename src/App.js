a56ea7dbeca55c6da1fe74f524089c5bbbda4b53
Matt Huntington <matt.huntington@gmail.com>
2021-02-27 10:21:21 -0500

    basic html

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+	render = ()=>{
+		return <main>
+			<h1>Contacts App</h1>
+			<section>
+				<h2>List of Current Conacts</h2>
+				<ul>
+				</ul>
+			</section>
+			<section>
+				<h2>Create a new Contact</h2>
+				<form>
+					Name: <input type="text"/><br/>
+					Age: <input type="number"/><br/>
+					<input type="submit" value="Create Contact" />
+				</form>
+			</section>
+		</main>
+	}
+
 }
 
 export default App;
