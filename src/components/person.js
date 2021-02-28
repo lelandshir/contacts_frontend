70398ce2c016cf68bc7c2fdecf4619f426696356
Matt Huntington <matt.huntington@gmail.com>
2021-02-27 19:43:14 -0500

    update functionality

--- src/components/person.js
+++ src/components/person.js
@@ -11,10 +11,42 @@ class Person extends React.Component {
 				this.props.refreshCallback();
 			})
 	}
+
+	updatePerson = (event)=>{
+		event.preventDefault();
+		axios
+			.put(
+				`https://desolate-thicket-29906.herokuapp.com/api/contacts/${this.props.data.id}`,
+				{
+					name:this.state.name,
+					age:this.state.age
+				}
+			).then(()=>{
+				this.props.refreshCallback();
+			})
+	}
+
+	changeName = (event)=>{
+		this.setState({
+			name:event.target.value
+		})
+	}
+
+	changeAge = (event)=>{
+		this.setState({
+			age:event.target.value
+		})
+	}
+
 	render = ()=>{
 		return <li key={this.props.data.id}>
 			{this.props.data.name} is {this.props.data.age} years old
 			<button onClick={this.deletePerson}>Delete</button>
+			<form onSubmit={this.updatePerson}>
+				<input type="text" onKeyUp={this.changeName} />
+				<input type="number" onKeyUp={this.changeAge} />
+				<input type="submit" value="Update"/>
+			</form>
 		</li>
 	}
 }
